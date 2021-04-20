db7cfa097a658d6dbe78bbc2ac0c34a4c0f94ecc
rachitrai99 <54245123+rachitrai99@users.noreply.github.com>
2021-04-20 18:25:19 +0530

    upload

---
+++ ScreenHoncho-MiniProject/app/src/main/java/me/screenHonchoteam/screenhoncho/NavigationDrawerItem.java
@@ -0,0 +1,15 @@
+  
+package me.screenHonchoteam.screenhoncho.;
+
+/**
+ * Created by rohit
+ */
+public class NavigationDrawerItem {
+	public int icon;
+	public String title;
+	
+	public NavigationDrawerItem (int icon, String title) {
+		this.icon = icon;
+		this.title = title;
+	}
+}
